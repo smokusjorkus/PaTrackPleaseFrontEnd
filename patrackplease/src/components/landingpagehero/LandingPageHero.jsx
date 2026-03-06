421f523101e37f277257ee6b632ef4a62a69a25d
smokusjorkus <johannelumauag11@gmail.com>
2026-03-06 12:56:42 +0800

    changed backgrounds of the landingpage

--- patrackplease/src/components/landingpagehero/LandingPageHero.jsx
+++ patrackplease/src/components/landingpagehero/LandingPageHero.jsx
@@ -6,23 +6,20 @@ import { Link } from "react-router-dom";
 export default function LandingPageHero() {
   return (
     <section className="hero-first">
-      <div
-        className="hero-content animate__animated animate__fadeInUp 
-      "
-      >
+      <div className="hero-content animate__animated animate__fadeInUp">
         <h1 className="hero-title">PaTrack Please</h1>
 
         <p className="hero-subtitle animate__animated animate__fadeInUp animate__delay-1s">
           Your <span className="BOLD">Only</span> To-do App
         </p>
 
-        <div className="hero-buttons">
-          <button className="primary-btn">
-            <Link to="/Login">Login</Link>
-          </button>
-          <button className="secondary-btn">
-            <Link to="/Register">Register</Link>
-          </button>
+        <div className="hero-buttons animate__animated animate__fadeInUp animate__delay-2s">
+          <Link to="/Login" className="primary-btn">
+            Login
+          </Link>
+          <Link to="/Register" className="secondary-btn">
+            Register
+          </Link>
         </div>
       </div>
     </section>
