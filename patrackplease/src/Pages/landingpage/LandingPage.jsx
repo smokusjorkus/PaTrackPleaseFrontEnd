c9d8aa59f089f6d03ca014d9397df32736812136
smokusjorkus <johannelumauag11@gmail.com>
2026-04-27 22:32:46 +0800

    fixed import NavBar casing

--- patrackplease/src/Pages/landingpage/LandingPage.jsx
+++ patrackplease/src/Pages/landingpage/LandingPage.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./LandingPageStyle.css";
-import NavBar from "../../components/navbar/navbar";
+import NavBar from "../../components/navbar/NavBar";
 import LandingPageHero from "../../components/landingpagehero/LandingPageHero";
 import AboutUs from "../../components/aboutus/AboutUs";
 import FeaturesHero from "../../components/features/FeaturesHero";
 import Footer from "../../components/footer/Footer";
-import Balatro from "../../components/balatro/Balatro";
 
 export default function LandingPage() {
   return (
